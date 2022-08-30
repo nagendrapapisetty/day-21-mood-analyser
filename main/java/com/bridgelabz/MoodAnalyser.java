a9c0f68e6b19debd5458f06ffc76300ebc582276
unknown <papisettynagendra>
2022-08-30 22:02:36 +0530

    Added Ability To Analyse

---
+++ main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class MoodAnalyser {
+    private String message;
+    public MoodAnalyser(String message)
+    {
+        this.message = message;
+    }
+    public MoodAnalyser()
+    {
+        this.message = null;
+    }
+    public String moodAnalyser()
+    {
+        if (this.message.contains("Sad"))
+            return "SAD";
+
+        return "HAPPY";
+    }
+
+}
+
---
+++ test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyserTest {
+    MoodAnalyser object ;
+    @BeforeEach
+    public void setUp()
+    {
+        object = new MoodAnalyser("This is a happy message");
+    }
+    @Test
+    public void moodAnalyser()
+    {
+        String ans = object.moodAnalyser();
+        Assertions.assertEquals("HAPPY",ans);
+        System.out.println(ans);
+    }
+}
+
